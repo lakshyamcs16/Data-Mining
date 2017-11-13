07340e8f5b746e786d243a3acde3811b99755aa9
lakshyamcs16 <32986427+lakshyamcs16@users.noreply.github.com>
2017-11-13 22:34:01 +0530

    Plot.ly Outputs

---
+++ Simple dendrogram/figure.js
@@ -0,0 +1,255 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": false, 
+        "yaxis": {
+            "showticklabels": true, 
+            "ticks": "outside", 
+            "showgrid": false, 
+            "mirror": "allticks", 
+            "zeroline": false, 
+            "showline": true, 
+            "rangemode": "tozero", 
+            "type": "linear"
+        }, 
+        "showlegend": false, 
+        "height": 800, 
+        "width": 1000, 
+        "xaxis": {
+            "showticklabels": true, 
+            "tickmode": "array", 
+            "ticks": "outside", 
+            "showgrid": false, 
+            "mirror": "allticks", 
+            "zeroline": false, 
+            "showline": true, 
+            "ticktext": [
+                "6", 
+                "5", 
+                "1", 
+                "0", 
+                "2", 
+                "3", 
+                "4", 
+                "7", 
+                "8"
+            ], 
+            "rangemode": "tozero", 
+            "type": "linear", 
+            "tickvals": [
+                5.0, 
+                15.0, 
+                25.0, 
+                35.0, 
+                45.0, 
+                55.0, 
+                65.0, 
+                75.0, 
+                85.0
+            ]
+        }, 
+        "hovermode": "closest"
+    }, 
+    "data": [
+        {
+            "yaxis": "y", 
+            "ysrc": "apooos3:39:c6c1e2", 
+            "text": null, 
+            "xsrc": "apooos3:39:756ef3", 
+            "marker": {
+                "color": "rgb(61,153,112)"
+            }, 
+            "mode": "lines", 
+            "xaxis": "x", 
+            "hoverinfo": "text", 
+            "y": [
+                0.0, 
+                0.18401939563254477, 
+                0.18401939563254477, 
+                0.0
+            ], 
+            "x": [
+                35.0, 
+                35.0, 
+                45.0, 
+                45.0
+            ], 
+            "type": "scatter"
+        }, 
+        {
+            "yaxis": "y", 
+            "ysrc": "apooos3:39:8471a9", 
+            "text": null, 
+            "xsrc": "apooos3:39:e876b0", 
+            "marker": {
+                "color": "rgb(61,153,112)"
+            }, 
+            "mode": "lines", 
+            "xaxis": "x", 
+            "hoverinfo": "text", 
+            "y": [
+                0.0, 
+                0.5436238103292539, 
+                0.5436238103292539, 
+                0.18401939563254477
+            ], 
+            "x": [
+                25.0, 
+                25.0, 
+                40.0, 
+                40.0
+            ], 
+            "type": "scatter"
+        }, 
+        {
+            "yaxis": "y", 
+            "ysrc": "apooos3:39:5b7b0b", 
+            "text": null, 
+            "xsrc": "apooos3:39:a84f47", 
+            "marker": {
+                "color": "rgb(61,153,112)"
+            }, 
+            "mode": "lines", 
+            "xaxis": "x", 
+            "hoverinfo": "text", 
+            "y": [
+                0.0, 
+                0.6527010493344537, 
+                0.6527010493344537, 
+                0.5436238103292539
+            ], 
+            "x": [
+                15.0, 
+                15.0, 
+                32.5, 
+                32.5
+            ], 
+            "type": "scatter"
+        }, 
+        {
+            "yaxis": "y", 
+            "ysrc": "apooos3:39:68718d", 
+            "text": null, 
+            "xsrc": "apooos3:39:830ca0", 
+            "marker": {
+                "color": "rgb(61,153,112)"
+            }, 
+            "mode": "lines", 
+            "xaxis": "x", 
+            "hoverinfo": "text", 
+            "y": [
+                0.0, 
+                0.8624908443607263, 
+                0.8624908443607263, 
+                0.6527010493344537
+            ], 
+            "x": [
+                5.0, 
+                5.0, 
+                23.75, 
+                23.75
+            ], 
+            "type": "scatter"
+        }, 
+        {
+            "yaxis": "y", 
+            "ysrc": "apooos3:39:46e274", 
+            "text": null, 
+            "xsrc": "apooos3:39:77164e", 
+            "marker": {
+                "color": "rgb(255,65,54)"
+            }, 
+            "mode": "lines", 
+            "xaxis": "x", 
+            "hoverinfo": "text", 
+            "y": [
+                0.0, 
+                1.271564987120556, 
+                1.271564987120556, 
+                0.0
+            ], 
+            "x": [
+                55.0, 
+                55.0, 
+                65.0, 
+                65.0
+            ], 
+            "type": "scatter"
+        }, 
+        {
+            "yaxis": "y", 
+            "ysrc": "apooos3:39:e4a24f", 
+            "text": null, 
+            "xsrc": "apooos3:39:9e0492", 
+            "marker": {
+                "color": "rgb(0,116,217)"
+            }, 
+            "mode": "lines", 
+            "xaxis": "x", 
+            "hoverinfo": "text", 
+            "y": [
+                0.0, 
+                1.3505807094752558, 
+                1.3505807094752558, 
+                0.0
+            ], 
+            "x": [
+                75.0, 
+                75.0, 
+                85.0, 
+                85.0
+            ], 
+            "type": "scatter"
+        }, 
+        {
+            "yaxis": "y", 
+            "ysrc": "apooos3:39:c7e3a9", 
+            "text": null, 
+            "xsrc": "apooos3:39:61568b", 
+            "marker": {
+                "color": "rgb(0,116,217)"
+            }, 
+            "mode": "lines", 
+            "xaxis": "x", 
+            "hoverinfo": "text", 
+            "y": [
+                1.271564987120556, 
+                1.389975016042114, 
+                1.389975016042114, 
+                1.3505807094752558
+            ], 
+            "x": [
+                60.0, 
+                60.0, 
+                80.0, 
+                80.0
+            ], 
+            "type": "scatter"
+        }, 
+        {
+            "yaxis": "y", 
+            "ysrc": "apooos3:39:9ec47e", 
+            "text": null, 
+            "xsrc": "apooos3:39:ed6c0a", 
+            "marker": {
+                "color": "rgb(0,116,217)"
+            }, 
+            "mode": "lines", 
+            "xaxis": "x", 
+            "hoverinfo": "text", 
+            "y": [
+                0.8624908443607263, 
+                1.8902497336232362, 
+                1.8902497336232362, 
+                1.389975016042114
+            ], 
+            "x": [
+                14.375, 
+                14.375, 
+                70.0, 
+                70.0
+            ], 
+            "type": "scatter"
+        }
+    ]
+}
